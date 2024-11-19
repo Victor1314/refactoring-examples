fd58a1343420a62072e91cd689c6f6b35c186004
yuanhui.chen <972682194@qq.com>
2024-11-19 16:23:27 +0800

    feat: add replace array with object

---
+++ simple/golang/replace-array-with-object_after.go
@@ -0,0 +1,9 @@
+
+
+row := Performance()
+row.setName("Liverpool")
+row.setWins("15")
+
+// row = Performance()
+// row.setName("Liverpool")
+// row.setWins("15")
---
+++ simple/golang/replace-array-with-object_before.go
@@ -0,0 +1,10 @@
+
+
+
+var row = [2]string{}
+row[0] = "Liverpool"
+row[1] = "15"
+
+// row = [None * 2]
+// row[0] = "Liverpool"
+// row[1] = "15"
