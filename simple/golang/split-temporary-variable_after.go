6e5dcf0b1d7ba4247cda781567e4a609f215fad1
yuanhui.chen <972682194@qq.com>
2024-11-19 16:25:09 +0800

    feat: add split-temporary_variable

---
+++ simple/golang/split-temporary-variable_after.go
@@ -0,0 +1,13 @@
+
+
+
+perimeter := 2 * (height + width)
+fmt.Println(perimeter)
+
+area := height * width
+fmt.Println(area)
+
+// perimeter = 2 * (height + width)
+// print(perimeter)
+// area = height * width
+// print(area)
---
+++ simple/golang/split-temporary-variable_before.go
@@ -0,0 +1,13 @@
+
+
+
+
+temp := 2 * (height + width)
+fmt.Println(temp)
+temp = height * width
+fmt.Println(temp)
+
+// temp = 2 * (height + width)
+// print(temp)
+// temp = height * width
+// print(temp)
