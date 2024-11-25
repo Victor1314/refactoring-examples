67d79da8bdf41cbf6225966fd23ea1485b2a1daa
yuanhui.chen <972682194@qq.com>
2024-11-25 10:52:41 +0800

    feat: replace magic number with symbolic constant

---
+++ simple/golang/replace-magic-number-with-symbolic-constant_after.go
@@ -0,0 +1,12 @@
+
+
+const GRAVITATIONAL_CONSTANT = 9.81
+
+func potentialEnergy(mass, height float64) float64 {
+	return mass * height * GRAVITATIONAL_CONSTANT
+}
+
+// GRAVITATIONAL_CONSTANT = 9.81
+
+// def potentialEnergy(mass, height):
+//     return mass * height * GRAVITATIONAL_CONSTANT
---
+++ simple/golang/replace-magic-number-with-symbolic-constant_before.go
@@ -0,0 +1,9 @@
+
+
+
+func potentialEnergy(mass, height float64) float64 {
+	return mass * height * 9.81
+}
+
+// def potentialEnergy(mass, height):
+//     return mass * height * 9.81
