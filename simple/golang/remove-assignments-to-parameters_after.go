6e8d4e07ac28287fd7554550ea9a960ca7f5c347
yuanhui.chen <972682194@qq.com>
2024-11-26 20:50:16 +0800

    feat: add remove  assignments to parameters

---
+++ simple/golang/remove-assignments-to-parameters_after.go
@@ -0,0 +1,16 @@
+
+
+
+func discount(inputVal int, quantity int) {
+	result := inputVal
+	if quantity > 50 {
+		result -= 2
+	}
+	// ...
+}
+
+// def discount(inputVal, quantity):
+//     result = inputVal
+//     if quantity > 50:
+//         result -= 2
+//     # ...
---
+++ simple/golang/remove-assignments-to-parameters_before.go
@@ -0,0 +1,15 @@
+
+
+
+func discount(inputVal int, quantity int) {
+	if quantity > 50 {
+		inputVal -= 2
+	}
+
+	// ...
+}
+
+// def discount(inputVal, quantity):
+//     if quantity > 50:
+//         inputVal -= 2
+//     # ...
